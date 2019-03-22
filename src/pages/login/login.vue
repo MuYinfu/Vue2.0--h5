094e1744ad96ecb2cd8e7a11dfa200948745c0ab
muyinfu <2298286002@qq.com>
2019-03-22 20:59:32 +0800

    基本路由跳转搭建

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,15 +1,33 @@
 <template>
-    <div class='warp'>
-        这里是登入组件
+    <div class='warp' @click='getHellowWorld'>
+        {{msg}}
     </div>
 </template>
 
 <script>
-    export default {
-        name: "login"
+  export default {
+      name: "login",
+    data() {
+        return {
+          msg: '这里是登入路由！'
+        }
+    },
+    methods:{
+      getHellowWorld(){
+        this.$router.push({path : '/'})
+      }
+    },
+    watch:{
+
     }
+  }
 </script>
 
-<style scoped>
+<style lang='less'>
+@import "../../styles/index.less";
+@import "../../styles/variable.less";
 
+.warp{
+  .mt(20)
+}
 </style>
